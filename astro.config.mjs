4e1927aaea26d43781fb02dfff3d576dd42170c9
Jonas Bulik <jonas@bulik.dev>
2025-10-22 18:08:20 +0200

    docs(guides): add project structure guide placeholder

--- astro.config.mjs
+++ astro.config.mjs
@@ -24,6 +24,7 @@ export default defineConfig({
           items: [
             // Each item here is one entry in the navigation menu.
             { label: "Example Guide", slug: "guides/example" },
+            { label: "Project Structure", slug: "guides/project-structure" },
           ],
         },
         {
